4e16857c414a1f724dc69ff28d04dfc26a555c1c
jeydn <eiskristall93@yahoo.de>
2018-04-25 13:17:51 +0000

    App frame implemented

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import { TabNavigator, TabBarBottom, StackNavigator} from 'react-navigation';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 class ScanScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Scan',
+  }
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
@@ -14,6 +17,9 @@ class ScanScreen extends React.Component {
 }
 
 class BoxesScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Boxes',
+  }
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
@@ -24,6 +30,14 @@ class BoxesScreen extends React.Component {
 }
 
 class DetailsScreen extends React.Component {
+  //Title includes name of box
+  static navigationOptions = ({ navigation }) => {
+    const { params } = navigation.state;
+
+    return {
+      title: params ? params.otherParam : 'Unnamed Box',
+    }
+  };
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
@@ -33,49 +47,52 @@ class DetailsScreen extends React.Component {
   }
 }
 
-const HomeStack = StackNavigator({
-  Scan: { screen: ScanScreen },
-  Boxes: { screen: BoxesScreen },
-  Details: { screen: DetailsScreen },
-});
+const tab = TabNavigator(
+ {
+   Scan: { screen: ScanScreen },
+   Boxes: { screen: BoxesScreen },
+   Details: { screen: DetailsScreen },
+ },
+ {
+   navigationOptions: ({ navigation }) => ({
+     tabBarIcon: ({ focused, tintColor }) => {
+       const { routeName } = navigation.state;
+       let iconName;
+       if (routeName === 'Scan') {
+         iconName = `ios-qr-scanner${focused ? '' : '-outline'}`;
+       } else if (routeName === 'Boxes') {
+         iconName = `ios-cube${focused ? '' : '-outline'}`;
+       } else if (routeName === 'Details') {
+         iconName = `ios-list-box${focused ? '' : '-outline'}`;
+       }
 
-const SettingsStack = StackNavigator({
-  Scan: { screen: ScanScreen },
-  Boxes: { screen: BoxesScreen },
-  Details: { screen: DetailsScreen },
-});
-
-export default TabNavigator(
-  {
-    Scan: { screen: ScanScreen },
-    Boxes: { screen: BoxesScreen },
-    Details: { screen: DetailsScreen },
-  },
-  {
-    navigationOptions: ({ navigation }) => ({
-      tabBarIcon: ({ focused, tintColor }) => {
-        const { routeName } = navigation.state;
-        let iconName;
-        if (routeName === 'Scan') {
-          iconName = `ios-qr-scanner${focused ? '' : '-outline'}`;
-        } else if (routeName === 'Boxes') {
-          iconName = `ios-cube${focused ? '' : '-outline'}`;
-        } else if (routeName === 'Details') {
-          iconName = `ios-list-box${focused ? '' : '-outline'}`;
-        }
-
-        // You can return any component that you like here! We usually use an
-        // icon component from react-native-vector-icons
-        return <Ionicons name={iconName} size={25} color={tintColor} />;
-      },
-    }),
-    tabBarOptions: {
-      activeTintColor: '#004880',
-      inactiveTintColor: 'gray',
-    },
-    tabBarComponent: TabBarBottom,
-    tabBarPosition: 'bottom',
-    animationEnabled: false,
-    swipeEnabled: true,
-  }
+       // You can return any component that you like here! We usually use an
+       // icon component from react-native-vector-icons
+       return <Ionicons name={iconName} size={25} color={tintColor} />;
+     },
+   }),
+   tabBarOptions: {
+     activeTintColor: '#004880',
+     inactiveTintColor: 'gray',
+   },
+   tabBarComponent: TabBarBottom,
+   tabBarPosition: 'bottom',
+   animationEnabled: false,
+   swipeEnabled: true,
+ }
 );
+
+export default StackNavigator({
+   MyTab: {
+     screen: tab,
+     navigationOptions: {
+       headerStyle: {
+        backgroundColor: '#2E4761',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    },
+  }
+})
